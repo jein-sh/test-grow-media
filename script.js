603f123c6006c8ef9e1d48b22f7552f1b7787fa5
jein-sh <jein.sh85@gmail.com>
2023-12-01 15:37:34 +0200

    добавляет маску на телефон

--- script.js
+++ script.js
@@ -2,6 +2,47 @@ const body = document.querySelector('body');
 const form = document.querySelector('form');
 const modal = document.querySelector('.modal');
 
+const phoneMask = () => {
+    const phone = document.getElementById('user-phone');
+
+    phone.addEventListener('keypress', (evt) => {
+        if (evt.keyCode < 47 || evt.keyCode > 57) {
+          evt.preventDefault();
+        }
+        if (phone.value.length === 0) {
+          phone.value = '+7 (';
+          phone.selectionStart = phone.value.length;
+        }
+        if (phone.value.length === 7) {
+          phone.value += ') ';
+        }
+    });
+      
+    phone.addEventListener('click', (evt) => {
+        if (phone.selectionStart < 4) {
+          phone.selectionStart = phone.value.length;
+        }
+        if (evt.key === 'Backspace' && phone.value.length <= 4) {
+          evt.preventDefault();
+        }
+    });
+      
+    phone.addEventListener('blur', () => {
+        if (phone.value === '+7(') {
+          phone.value = '';
+        }
+    });
+      
+    phone.addEventListener('keydown', (evt) => {
+        if (evt.key === 'Backspace' && phone.value.length <= 4) {
+          evt.preventDefault();
+        }
+        if (evt.key === 'ArrowLeft' && phone.value.length <= 4) {
+          evt.preventDefault();
+        }
+    });
+}
+
 const initModal = () => {
     if (!modal) {
         return;
@@ -38,6 +79,7 @@ const initForm = () => {
         return;
     }
 
+    phoneMask();
     const btnSubmit = form.querySelector('.form__btn');
 
    form.addEventListener('submit', (evt) => {
@@ -57,3 +99,4 @@ const initForm = () => {
 }
 
 initForm();
+
